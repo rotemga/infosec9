42f46c9c452662483d73b5386e3e1d70e01aec7a
user <rotem.ge100@gmail.com>
2017-06-02 17:51:59 +0300

    change q1b

--- q1/q1b.py
+++ q1/q1b.py
@@ -1,5 +1,6 @@
 from scapy.all import *
 import os
+import datetime
 
 
 count_syn_dict = dict()
@@ -8,45 +9,59 @@ timer = 0
 
 
 def on_packet(packet):
-	
-	is_syn = 0
 
+	is_syn = 0
+	now = datetime.datetime.now()
+
+	if (not (packet.haslayer (TCP))):
+		return
+		
 	#check if it SYN packet
-	if (packet.hasLayer (TCP)):
-		F = packet['TCP'].flags
-		if (F & SYN): 
-			is_syn = 1
+	F = packet['TCP'].flags
+	if (F & SYN): 
+		is_syn = 1
 
 	ip = packet[IP].src
+
+	clean_time (ip, now)
+
 
 	if (is_blocked(ip) and is_syn == 1):
 		os.system ("iptables -A INPUT -s " + ip +" -j DROP")
 		os.system ("iptables -A OUTPUT -d " + pi + " -j DROP")
+		print ip
 
 	#add to dict
-	add_to_dict( ip, is_syn)
+	if (is_syn):
+		add_to_dict( ip, now)
 	pass 
 
 
 def is_blocked(ip):
-	num_of_syn_in_last_minute = sum(count_syn_dict[ip])
-	return (num_of_syn_in_last_minute > 15)
-
-
-def add_to_dict(ip, syn):
 	if not (ip in count_syn_dict):
-		list = [0] * 60 #create list of size 60 with zeros.
+		return False
+	
+	return (len (count_syn_dict[ip]) > 15)
+
+
+def add_to_dict(ip, now):
+	if not (ip in count_syn_dict):
+		list = [] #create empty list.
 		count_syn_dict[ip] = list
-	count_syn_dict[ip][timer] = syn #update the list with the message
-	update_timer ()
+	count_syn_dict[ip].append(now) #update the list with the message
 
 
 
 
 
-def update_timer ():
-	timer = timer + 1
-	timer = timer % 60 #mod to timer to be from 0 to 59.
+def clean_time (ip, now):
+	timeList = count_syn_dict[ip]
+	for i in range (0, len(timeList)):
+		delta = now - timeList[i]
+		time_diff = divmod(delta.days * 86400 + delta.seconds, 60)
+		if (time_diff[1] > 15):
+			count_syn_dict[ip].remove(timeList[i])
+	
 
 
 def main():
