0127ca7ac4f240cf50e27728621f94db21f6be47
user <rotem.ge100@gmail.com>
2017-06-03 12:08:03 +0300

    q2b1+q2a2

--- q2/b/bigbrother.py
+++ q2/b/bigbrother.py
@@ -1,11 +1,24 @@
 from scapy.all import *
 
-
 unpersons = set()
 
 
 def spy(packet):
-    pass # Reimplement me! (b1)
+	if not packet.haslayer(TCP):
+		return
+
+	payload = str(packet[TCP].payload)
+	sender_ip = packet[IP].src
+	
+	distribution = [float(payload.count(c)) / len(payload) for c in set(payload)]
+	entropy = -sum(p * math.log(p)/math.log(2.0) for p in distribution)
+
+	if ('love' in payload) or (entropy > 3.0):
+		print entropy
+		unpersons.add (sender_ip)
+		
+
+	pass
 
 
 def main():
